33f7fcfd56f38d8353f052c2342fe8249f1f8edf
David Weldon <dweldon@gmail.com>
2014-09-26 12:32:53 -0700

    Updated packages.

--- includes/packages.sh
+++ includes/packages.sh
@@ -8,7 +8,7 @@ packages.execute() {
   sudo apt-get -y upgrade > /dev/null
 
   local package
-  local packages=(chromium-browser build-essential libssl-dev curl ntp tree meld tofrodos mediainfo)
+  local packages=(chromium-browser pepperflashplugin-nonfree build-essential libssl-dev curl ntp tree meld tofrodos mediainfo gir1.2-gtop-2.0)
   for package in "${packages[@]}"; do
     util.installPackage $package
   done
