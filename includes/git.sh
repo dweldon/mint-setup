876735f0c05b31004fbd1edb543aae5a0d72b36a
David Weldon <dweldon@gmail.com>
2015-06-23 10:28:40 -0700

    Added git merge options and changed the push default.

--- includes/git.sh
+++ includes/git.sh
@@ -12,7 +12,10 @@ git.execute() {
   git config --global user.name "$name"
   git config --global user.email "$email"
   git config --global color.ui auto
-  git config --global push.default matching
+  git config --global push.default simple
+  git config --global diff.tool meld
+  git config --global merge.tool meld
+  git config --global difftool.prompt false
   git config --global alias.hist 'log --pretty=format:"%C(yellow)%h%C(reset) %ad %C(green)%s%C(reset) %C(magenta)[%an]%C(reset)%d" --graph --date=short'
   git config --global alias.cleanup '!git remote prune origin && git gc'
   util.assertExecutable 'git'
