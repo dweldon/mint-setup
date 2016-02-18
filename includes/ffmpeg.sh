1c1db26ba3e943bc92902c5baa22825d76e3d267
David Weldon <dweldon@gmail.com>
2016-02-18 15:07:06 -0800

    Updated ffmpeg.

--- includes/ffmpeg.sh
+++ includes/ffmpeg.sh
@@ -1,14 +1,20 @@
 ffmpeg.execute() {
   util.message 'installing ffmpeg'
 
-  local filename='ffmpeg.static.32bit.latest.tar.gz'
+  local filename='ffmpeg-release-32bit-static.tar.xz'
+  local dirname='ffmpeg'
 
-  util.wget "http://ffmpeg.gusari.org/static/32bit/${filename}"
-  tar -xzf "$filename"
+  util.wget "http://johnvansickle.com/ffmpeg/releases/${filename}"
+  mkdir "$dirname"
+  tar -C "$dirname" --strip-components 1 -xf "$filename"
+
+  cd "$dirname"
   sudo chown root:root ffmpeg ffprobe
   sudo mv ffmpeg /usr/local/bin/
   sudo mv ffprobe /usr/local/bin/
+  cd ../
   rm "$filename"
+  rm -rf "$dirname"
 
   util.assertExecutable 'ffmpeg'
   util.assertExecutable 'ffprobe'
