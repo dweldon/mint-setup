bf97bcdd151a338c6481e55d977486a7c632f0c9
David Weldon <dweldon@gmail.com>
2016-02-18 14:20:03 -0800

    Removed flash.

--- includes/packages.sh
+++ includes/packages.sh
@@ -2,7 +2,7 @@ packages.execute() {
   util.message 'installing packages'
 
   local package
-  local packages=(chromium-browser pepperflashplugin-nonfree build-essential libssl-dev curl ntp tree meld tofrodos mediainfo gir1.2-gtop-2.0)
+  local packages=(chromium-browser build-essential libssl-dev curl ntp tree meld tofrodos mediainfo gir1.2-gtop-2.0)
   for package in "${packages[@]}"; do
     util.installPackage $package
   done
